0020ab109ff1281c070d1cbf8dabc9bb472a24b0
mohamed youssef <mohamed.youssef@objects.com>
2024-06-15 00:14:11 +0300

    create new url for search in msgs

--- chat/api/urls.py
+++ chat/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views.room import rooms_view, single_room_view , room_count_view
-from .views.message import message_view, single_message_view
+from .views.message import message_view, single_message_view, search_message_view
 
 urlpatterns = [
     path('rooms/', rooms_view, name="rooms_view"),
     path('rooms/count/', room_count_view, name="room_count_view"),
     path('rooms/<int:id>/', single_room_view, name="single_room_view"),
     path('rooms/<int:id>/message/', message_view, name="message_view"),
-    path('rooms/<int:room_id>/message/<int:id>/', single_message_view, name="single_message_view")
+    path('rooms/<int:room_id>/message/<int:id>/', single_message_view, name="single_message_view"),
+    path('rooms/<int:id>/search/', search_message_view, name="search_message_view"),
 ]
